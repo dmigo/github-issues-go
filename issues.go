eb839679c62f8e70d923becd45474ad1b8076b9e
dmigo <d.f.goryunov@gmail.com>
2019-03-13 07:57:05 +0100

    Get github issues for a project

--- issues.go
+++ issues.go
@@ -1,14 +1,49 @@
 package main
 
-import "net/http"
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func GetUrl() string {
+
+	const BASE_URI = "https://api.github.com"
+	const OWNER = "gotify"
+	const PROJECT_NAME = "server"
+	const BUG_LABEL = "a:bug"
+
+	var Url *url.URL
+	Url, err := url.Parse(BASE_URI)
+
+	if err != nil {
+		panic(fmt.Sprintf("bad url: %s", BASE_URI))
+	}
+
+	Url.Path += "/repos/" + OWNER + "/" + PROJECT_NAME + "/issues"
+	parameters := url.Values{}
+	parameters.Add("labels", BUG_LABEL)
+	Url.RawQuery = parameters.Encode()
+
+	return Url.String()
+}
 
 func main() {
-	resp, err := http.Get("http://example.com/")
+	var url = GetUrl()
+	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	} else {
-		fmt.Printf("result: %v\n", resp)
+		bodyBytes, readErr := ioutil.ReadAll(resp.Body)
+
+		if readErr != nil {
+			fmt.Printf("read error: %v\n", err)
+		} else {
+			bodyString := string(bodyBytes)
+			fmt.Printf("result: %v\n", resp.StatusCode)
+			fmt.Printf("result: %v\n", bodyString)
+		}
 	}
 
 	fmt.Println("termingating")
