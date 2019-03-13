05d9f2d573af19fb24b37663c404c7a41373a6c5
dmigo <d.f.goryunov@gmail.com>
2019-03-13 07:14:21 +0100

    Primitive get request

--- issues.go
+++ issues.go
@@ -1,9 +1,15 @@
 package main
 
-// import "net/http"
+import "net/http"
 import "fmt"
 
 func main() {
-	// resp, err := http.Get("http://example.com/")
-	fmt.Println("Hello World!")
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	} else {
+		fmt.Printf("result: %v\n", resp)
+	}
+
+	fmt.Println("termingating")
 }
